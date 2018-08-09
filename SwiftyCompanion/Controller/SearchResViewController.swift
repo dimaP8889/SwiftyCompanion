3d23b0e6a40a84626d2da77830037c3cdaee0e54
Dmytro POGREBNIAK <dpogrebn@e1r3p4.unit.ua>
2018-08-09 04:07:14 +0300

    Get acces token

--- SwiftyCompanion/Controller/SearchResViewController.swift
+++ SwiftyCompanion/Controller/SearchResViewController.swift
@@ -7,13 +7,19 @@
 //
 
 import UIKit
+import SwiftyJSON
+import Alamofire
 
 class SearchResViewController: UIViewController {
-
+    
+    let UID = "d7d492c61c7fdff3ba700084416e5a68511b5b898598da227c9ab541ddb20941"
+    let SecretCode = "81617b0caa2f631e32492aa3015e05c4e57441f66dfba16ab621c6a5e2b7db57"
+    let tokenUrl = "https://api.intra.42.fr/oauth/token"
+    
+    var token = ""
     var NickName = ""
-    var NameSurname = ""
-    var Number = 0
-    var Points = 0
+    
+    let Data = UserData()
     
     @IBOutlet weak var tableView: UITableView!
     
@@ -22,8 +28,7 @@ class SearchResViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        setLabelNames()
-        configTable()
+        getToken()
         self.tableView.delegate = self
         self.tableView.dataSource = self
         self.tableView.register(TableViewCell.self, forCellReuseIdentifier: "TableViewCell")
@@ -39,6 +44,21 @@ class SearchResViewController: UIViewController {
         tableView.rowHeight = UITableViewAutomaticDimension
         tableView.estimatedRowHeight = 120.0
     }
+    
+    func    getToken() {
+        
+        let params:[String:String] = ["grant_type" : "client_credentials", "client_id" : UID, "client_secret" : SecretCode]
+        Alamofire.request(tokenUrl, method : .post, parameters : params).responseJSON {
+            response in
+            if response.result.isSuccess {
+                
+                let token : JSON = JSON(response.result.value!)
+                self.token = token["access_token"].stringValue
+            } else {
+                print ("Loshara Tupoi")
+            }
+        }
+    }
 
 
 }
@@ -52,8 +72,6 @@ extension SearchResViewController: UITableViewDelegate, UITableViewDataSource, U
         let cell = Bundle.main.loadNibNamed("TableViewCell", owner: self, options: nil)?.first as! TableViewCell
         
         cell.ProjectName.text = "lol"
-        
-        //cell.backgroundColor = .black
         
         return cell
     }
---
+++ SwiftyCompanion/Data/42Data.swift
@@ -0,0 +1,20 @@
+//
+//  42Data.swift
+//  SwiftyCompanion
+//
+//  Created by Dmytro POGREBNIAK on 09.08.2018.
+//  Copyright © 2018 Dmytro POGREBNIAK. All rights reserved.
+//
+
+import Foundation
+
+class UserData {
+    
+    var NickName = ""
+    var NameSurname = ""
+    var Number = 0
+    var Points = 0
+    var Level = 0
+    
+    
+}
